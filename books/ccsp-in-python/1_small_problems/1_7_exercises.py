951a7bac220fc5f850a82d99a897e683bc635e0c
Patryk K <pa.ko@wp.pl>
2019-10-14 23:02:39 +0200

    wip

--- books/ccsp-in-python/1_small_problems/1_7_exercises.py
+++ books/ccsp-in-python/1_small_problems/1_7_exercises.py
@@ -5,13 +5,11 @@ using a technique of your own design. Write unit tests that evaluate its correct
 and performance relative to the other versions in this chapter.
 """
 
-
 def fib(n: int) -> int:
     if n < 2:
         return n
     else:
         return fib(n-2) + fib(n-1)
-
 
 assert fib(7) == 13
 assert fib(10) == 55
@@ -19,6 +17,7 @@ assert fib(1) == 1
 assert fib(2) == 1
 assert fib(3) == 2
 print('all good')
+
 
 #%% 2.
 """
@@ -29,19 +28,102 @@ CompressedGene, using the wrapper.
 """
 
 
-class IntBitter(int):
+class SequenceOfBits:
 
-    def __int__(self):
-        pass
+    def __init__(self):
+        self.data: int = 1
+        self.n: int = -1
+
+    @property
+    def bit_length(self):
+        return self.data.bit_length() - 1
+
+    @property
+    def element_number(self):
+        return self.bit_length // 2
+
+    def add(self, value):
+        self.data <<= 2
+        self.data |= value
 
     def __iter__(self):
-        pass
+        self.n = -1
+        return self
 
     def __next__(self):
-        pass
+        self.n += 1
+        if self.n > self.element_number:
+            raise StopIteration
+        return self.__getitem__(self.n)
 
-    def __getitem__(self):
-        pass
+    def __repr__(self):
+        return bin(self.data)
+
+    def __getitem__(self, element_no):
+        pos = element_no*2
+        if element_no <= self.element_number:
+            return self.data >> pos & 0b11
+        raise ValueError('Out of index')
+
+# test
+sob = SequenceOfBits()
+sob.add(0b00)
+sob.add(0b01)
+sob.add(0b10)
+sob.add(0b11)
+
+print(sob)
+
+for n, s in enumerate(sob):
+    assert s == sob[n]
+
+class CompressedGene:
+
+    def __init__(self, gene) -> None:
+        self.bit_string: SequenceOfBits = SequenceOfBits()
+        self._compress(gene)
+
+    def _compress(self, gene: str) -> None:
+        for nucleotide in gene.upper():
+            if nucleotide == 'A':
+                self.bit_string.add(0b00)
+            elif nucleotide == 'C':
+                self.bit_string.add(0b01)
+            elif nucleotide == 'G':
+                self.bit_string.add(0b10)
+            elif nucleotide == 'T':
+                self.bit_string.add(0b11)
+            else:
+                raise ValueError(f'Invalid Nucleotide: {nucleotide}')
+
+    def decompress(self) -> str:
+        gene: str = ''
+
+        for bits in self.bit_string:
+            if bits == 0b00:
+                gene += 'A'
+            elif bits == 0b01:
+                gene += 'C'
+            elif bits == 0b10:
+                gene += 'G'
+            elif bits == 0b11:
+                gene += 'T'
+            else:
+                raise ValueError(f'Invalid bits: {bits}')
+        return gene[::-1]
+
+from sys import getsizeof
+
+original: str = "TAGGGATTAACCGTTATATATATATAGCCATGGATCGATTATATAGGGATTAACCGTTATATATATATAGCCATGGATCGATTATA"
+print('original is {} bytes'.format(getsizeof(original)))
+compressed: CompressedGene = CompressedGene(original)
+print('compresed is {} bytes'.format(getsizeof(compressed)))
+print('are the same: {}'.format(original == compressed.decompress()))
+print(original)
+print(compressed.decompress())
 
 
-#%%
+#%% 3.
+
+
+#%% 4.
