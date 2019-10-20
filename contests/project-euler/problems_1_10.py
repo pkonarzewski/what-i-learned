02c5b1da0a15ff43f9499680462ea35bbdbed8ad
Patryk K <pa.ko@wp.pl>
2019-10-20 13:32:49 +0200

    init project euler

---
+++ contests/project-euler/problems_1_10.py
@@ -0,0 +1,16 @@
+#%% Problem 1
+"""
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000.
+"""
+
+def problem1(n):
+    return None
+
+
+print(problem1(10))
+assert problem1(10) == 23, 'wrong'
+
+
+#%% Problem 2
