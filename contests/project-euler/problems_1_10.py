143d3ff029acbc147153d7db94e24982a1c4f430
pkonarzewski <pa.ko@wp.pl>
2019-10-24 23:14:06 +0200

    Fix name

--- contests/project-euler/problems_1_10.py
+++ contests/project-euler/problems_1_10.py
@@ -108,7 +108,7 @@ The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143 ?
 """
 
-def largest_factorial(n):
+def largest_prime_number_simple(n):
     return n
 
-print(largest_factorial(600851475143))
+print(largest_prime_number_simple(600851475143))
