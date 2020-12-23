0e2edb9416b2868d60155de271dc032f03136dcf
pkonarzewski <pa.ko@wp.pl>
2020-12-23 13:01:36 +0100

    Add counting sort

---
+++ algorithms/counting_sort.py
@@ -0,0 +1,19 @@
+#%%
+def counting_sort(input, k):
+    c = [0 for _ in range(k + 1)]  # O(k)
+    output = [None for _ in range(len(input))]  # O(n)
+
+    for num in input:  # O(n)
+        c[num] += 1
+
+    for i in range(1, len(c)):  # O(k)
+        c[i] += c[i-1]
+
+    for num in reversed(input):  # O(n)
+        output[c[num]-1] = num
+        c[num] -= 1
+
+    return output
+
+
+counting_sort([5,3,6,7,8,2,1,3,6,3,3,5], k=8)
