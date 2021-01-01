4cf7c546ef3887ba558ac1e9e3d75f5512b95d91
pkonarzewski <pa.ko@wp.pl>
2021-01-01 14:29:09 +0100

    Formating

--- algorithms/counting_sort.py
+++ algorithms/counting_sort.py
@@ -1,5 +1,9 @@
 #%%
 def counting_sort(input, k):
+    """
+    input - input array of non negative integers
+    k - range of key values
+    """
     c = [0 for _ in range(k)]  # O(k)
     output = [None for _ in range(len(input))]  # O(n)
 
@@ -7,15 +11,15 @@ def counting_sort(input, k):
         c[num] += 1
 
     for i in range(1, len(c)):  # O(k)
-        c[i] += c[i-1]
+        c[i] += c[i - 1]
 
     for num in reversed(input):  # O(n)
-        output[c[num]-1] = num
+        output[c[num] - 1] = num
         c[num] -= 1
 
     return output
 
 
-counting_sort([5,3,6,7,8,2,1,3,6,3,3,5], k=9)
+counting_sort([5, 3, 6, 7, 8, 2, 1, 3, 6, 3, 3, 5], k=9)
 
 # %%
