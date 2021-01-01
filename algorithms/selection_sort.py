974aeee70c812b04c6bbf1d3ab78bf74b6cbe0e6
pkonarzewski <pa.ko@wp.pl>
2021-01-01 15:44:30 +0100

    Formating

--- algorithms/selection_sort.py
+++ algorithms/selection_sort.py
@@ -6,9 +6,11 @@ def selection_sort(arr):
         for j in range(i + 1, len(arr)):
             if arr[j] < arr[min_val_index]:
                 min_val_index = j
+
         arr[i], arr[min_val_index] = arr[min_val_index], arr[i]
 
     return arr
+
 
 input_arr = [1, 4, 7, 2, 44, 7, 3, 21, 3]
 print(selection_sort(input_arr))
