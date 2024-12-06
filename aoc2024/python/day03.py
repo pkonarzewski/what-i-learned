7a6c01e8baef48b79350346f88b87af7629a5ac6
Patryk K <2836343+pkonarzewski@users.noreply.github.com>
2024-12-07 00:10:19 +0100

    day 3 part 2

--- aoc2024/python/day03.py
+++ aoc2024/python/day03.py
@@ -3,6 +3,8 @@ from pathlib import Path
 
 mul_pattern = re.compile(r"mul\((\d+),(\d+)\)")
 
+enabler = True
+
 
 def parse_mul(input: str, pos: int) -> tuple[int, int]:
     if match := mul_pattern.match(input[pos:]):
@@ -12,12 +14,23 @@ def parse_mul(input: str, pos: int) -> tuple[int, int]:
     return 1, 0
 
 
-def run(input: str) -> int:
+def run(input: str, use_enabler: bool = False) -> int:
     res = 0
     i = 0
-    while i < len(input) - 3:
-        scan = input[i : i + 3]
-        if scan == "mul":
+    global enabler
+
+    while i < len(input):
+        if input[i : i + 4] == "do()":
+            enabler = True
+            i += 3
+            continue
+
+        if input[i : i + 7] == "don't()":
+            enabler = False
+            i += 6
+            continue
+
+        if input[i : i + 3] == "mul" and (use_enabler is False or enabler):
             offset, mult = parse_mul(input, i)
             i += offset
             res += mult
@@ -32,6 +45,6 @@ if __name__ == "__main__":
     res = 0
     with (Path.cwd() / "input" / "day03-full").open(mode="r") as f:
         for line in f:
-            res += run(line)
+            res += run(line, use_enabler=True)
 
     print(res)
