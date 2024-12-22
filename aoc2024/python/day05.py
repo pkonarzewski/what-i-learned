733dd6858e1edf10c164507c417b54437d111e12
Patryk K <2836343+pkonarzewski@users.noreply.github.com>
2024-12-22 16:03:08 +0100

    aoc day5 1 python

---
+++ aoc2024/python/day05.py
@@ -0,0 +1,59 @@
+import sys
+from collections import defaultdict
+from pathlib import Path
+
+
+def load(path: Path) -> tuple[list[tuple[int, int]], list[tuple[int, ...]]]:
+    found_separator = False
+    page_order = []
+    pages = []
+
+    with path.open(mode="r") as f:
+        for line in f:
+            line = line.strip()
+            if line == "":
+                found_separator = True
+                continue
+
+            if not found_separator:
+                page_order.append(tuple(int(x) for x in line.split("|")))
+                continue
+
+            if found_separator:
+                pages.append(tuple(int(x) for x in line.split(",")))
+
+    return page_order, pages
+
+
+def is_correct_order(page_order: dict[int, set[int]], pages: tuple[int]) -> bool:
+    for i in range(len(pages) - 1):
+        node = page_order.get(pages[i], None)
+
+        if node is None:
+            return False
+
+        if not set(pages[i + 1 :]).issubset(node):
+            return False
+
+    return True
+
+
+def check_manual_order(page_order, pages_list) -> int:
+    answer = 0
+
+    order_graph = defaultdict(set)
+
+    for k, v in page_order:
+        order_graph[k].add(v)
+
+    for pages in pages_list:
+        if is_correct_order(order_graph, pages):
+            answer += pages[len(pages) // 2]
+
+    return answer
+
+
+if __name__ == "__main__":
+    page_order, pages = load(Path(sys.argv[1]))
+
+    print("Answer 1:", check_manual_order(page_order, pages))
